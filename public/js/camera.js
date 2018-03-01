7949fd214c6bb4fcfa1e56b3ccf2f62efefcc72e
Ellisa Lee <ehl024@ucsd.edu>
2018-03-01 10:57:50 -0800

    second time

--- public/js/camera.js
+++ public/js/camera.js
@@ -3,8 +3,7 @@ function initCamera() {
     capture($('#camera-video'),
           $('#camera-canvas'),
           $('#camera-button'));
-  
-
+    disableCamera();
 }
 
 function capture(video, canvas, snapshotButton) {
